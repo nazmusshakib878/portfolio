175a85f9235735ccdf63ab219af9f0f21c28d991
Shakib <nazmusshakib335@gmail.com>
2026-07-20 23:47:11 +0600

    Add theme provider and core dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import { ThemeProvider } from '@/context/theme-provider'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
