f4804f079595efb95f4f67f47ce9870af43367c7
nazmus878 <nazmusshakib335@gmail.com>
2026-08-05 13:36:31 +0600

    Allow Web3Forms in content security policy

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const contentSecurityPolicy=[
   "style-src 'self' 'unsafe-inline'",
   "img-src 'self' data: blob:",
   "font-src 'self' data:",
-  "connect-src 'self'",
+  "connect-src 'self' https://api.web3forms.com",
   "form-action 'self'",
   "frame-ancestors 'none'",
   "base-uri 'self'",
